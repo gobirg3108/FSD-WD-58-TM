65003e4e8dca774bd8290768f7b47280b74f4905
gobirg3108 <gobigobi3108@gmail.com>
2024-03-25 16:07:03 +0530

    update

---
+++ DAY - 3/index.js
@@ -0,0 +1,17 @@
+var myage = 21;
+var friend1 =myage;
+var friend2 =myage;
+friend2=myage+2;
+console.log("my age is :",myage);
+console.log("my friend 1 age is :",friend1);
+console.log("my friend 2 age is :",friend2);
+
+var mybike = {bike :"yamaha"};
+var myFriendbike = mybike;
+console.log("mybike is :",mybike.bike);
+console.log("my friend bike is :",myFriendbike.bike);
+
+mybike.bike="pulsur";
+console.log("mybike is after :",mybike.bike);
+console.log("my friend bike is after :",myFriendbike.bike);
+
--- DAY - 3/java.js
+++
@@ -1,3 +0,0 @@
-var name = "gobi";
-var name2 = "abarna";
-console.log(name+ " "+name2);
