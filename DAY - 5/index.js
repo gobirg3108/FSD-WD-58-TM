036aa24ab46c688b90b599470f79bab2a6c00586
gobirg3108 <gobigobi3108@gmail.com>
2024-03-28 15:40:32 +0530

    update

---
+++ DAY - 5/index.js
@@ -0,0 +1,29 @@
+const employees =[
+{name:"employee1",
+age : 28,
+department : "sales",
+email : "employee1@gmail.com"
+},
+
+{name:"employee2",
+age : 31,
+department : "development",
+email : "employee2@gmail.com"},
+
+{name:"employee3",
+age : 22,
+department : "management",
+email : "employee3@gmail.com"},
+
+{name:"employee4",
+age : 33,
+department : "sales",
+email : "employee4@gmail.com"}
+];
+createEmployeeReports(employees){
+    const reports = [];
+    for(let i = 0 ; i<employees.length; i++) {
+        const ( name , age, email ) = employees[i];
+    }
+    
+}
---
+++ DAY-4/index.js
@@ -0,0 +1,13 @@
+var assignName = function (){
+    return "Gobinath Ramalingam";
+
+};
+console.log(assignName());
+
+function cartDiscount(purchaseProducts,discount){
+    var amount = 3500;
+    var purchaseOffer = amount-discount;
+    return purchaseOffer
+}
+var PurchaseAmount = cartDiscount(200,800);
+ return(purchaseOffer)
