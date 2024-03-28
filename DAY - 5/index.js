93a0cf13dbb72c8ee2f0d0a02275d343f6cc9878
gobirg3108 <gobigobi3108@gmail.com>
2024-03-28 20:15:40 +0530

    update

--- DAY - 5/index.js
+++ DAY - 5/index.js
@@ -5,3 +5,13 @@ class Animal  {
     }
     moves = true;
 }
+class Rabbit extends Animal {
+    constructor () {
+        super ();
+        console.log("This is constructor from Rabbit class");
+
+    }
+    eats = true;
+}
+
+let r = new Rabbit ();
