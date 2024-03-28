6bb32d4600d3c6d34b56f63472b4339cccc559c1
gobirg3108 <gobigobi3108@gmail.com>
2024-03-28 19:58:31 +0530

    update

--- DAY - 5/index.js
+++ DAY - 5/index.js
@@ -1,29 +1,7 @@
-const employees =[
-{name:"employee1",
-age : 28,
-department : "sales",
-email : "employee1@gmail.com"
-},
+class Animal  {
+    constructor () {
+        console.log("This is constructor from Animal class");
 
-{name:"employee2",
-age : 31,
-department : "development",
-email : "employee2@gmail.com"},
-
-{name:"employee3",
-age : 22,
-department : "management",
-email : "employee3@gmail.com"},
-
-{name:"employee4",
-age : 33,
-department : "sales",
-email : "employee4@gmail.com"}
-];
-createEmployeeReports(employees){
-    const reports = [];
-    for(let i = 0 ; i<employees.length; i++) {
-        const ( name , age, email ) = employees[i];
     }
-    
+    moves = true;
 }
