bb5e26e3fbe685d830c5b550e8ce3619801815ab
gobirg3108 <gobigobi3108@gmail.com>
2024-03-31 14:04:47 +0530

    uodate

--- DAY - 5/index.js
+++ DAY - 5/index.js
@@ -11,6 +11,21 @@ class Rabbit extends Animal {
         console.log("This is constructor from Rabbit class");
 
     }
-    eats = true;
+    eats = true;}
+    
+    let lion = new Rabbit();
+
+var input = [23, 45, 678, 90, 33, 21, 780, 670, 498, 58940, 6545, 900, 370, 1299, 1300];
+
+var dividedByTen = input.filter(item => item % 10 === 0);
+
+console.log(dividedByTen); 
+
+
+function applyDiscount(a,b){
+var c = a-b;
+return c;
 }
+var finalAmount = applyDiscount(1000,500);
+console.log(finalAmount)
 
