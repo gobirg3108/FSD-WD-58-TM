5156251953e5c75ddaf6c80bd6c60c44b339a956
gobirg3108 <gobigobi3108@gmail.com>
2024-06-01 21:00:38 +0530

    update

--- DAY - 4/index.js
+++ DAY - 4/index.js
@@ -1,13 +1,12 @@
-var assignName = function (){
-    return "Gobinath Ramalingam";
-
+var assignName = function () {
+  return "Gobinath Ramalingam";
 };
 console.log(assignName());
 
-function cartDiscount(purchaseProducts,discount){
-    var amount = 3500;
-    var purchaseOffer = amount-discount;
-    return purchaseOffer
+function cartDiscount(purchaseProducts, discount) {
+  var amount = 3500;
+  var purchaseOffer = amount - discount;
+  return purchaseOffer;
 }
-var PurchaseAmount = cartDiscount(200,800);
- return(purchaseOffer)
+var PurchaseAmount = cartDiscount(200, 800);
+return purchaseOffer;
