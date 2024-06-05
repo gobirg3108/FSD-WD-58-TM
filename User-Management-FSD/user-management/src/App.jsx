35d692f041e6d5cdc43086e6430b875dd15ff651
gobirg3108 <gobigobi3108@gmail.com>
2024-06-05 22:43:05 +0530

    update

--- User-Management-FSD/user-management/src/App.jsx
+++ User-Management-FSD/user-management/src/App.jsx
@@ -1,15 +1,12 @@
-import { ThemeProvider, createTheme } from '@mui/material/styles'
-import { green, purple, orange } from '@mui/material/colors';
-import { CssBaseline, Container } from '@mui/material';
-import UserList from './UserList';
-import Register from './Register';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
-import Login from './Login';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { green, purple } from "@mui/material/colors";
+import { CssBaseline } from "@mui/material";
+import UserList from "./UserList";
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: orange[500],
+      main: purple[500],
     },
     secondary: {
       main: green[500],
@@ -19,20 +16,11 @@ const theme = createTheme({
 
 function App() {
   return (
-    <Router>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Container>
-          <Routes>
-            <Route path='/register' element={<Register />}></Route>
-            <Route path='/login' element={<Login />}></Route>
-            <Route path='/users' element={<UserList />}></Route>
-            <Route path='/' element={<Navigate to="/login"></Navigate>}></Route>
-          </Routes>
-        </Container>
-      </ThemeProvider >
-    </Router>
-  )
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <UserList />
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
--- User-Management-FSD/user-management/src/UserForm.jsx
+++ User-Management-FSD/user-management/src/UserForm.jsx
@@ -1,55 +1,67 @@
-import React, { useEffect, useState } from 'react'
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@mui/material'
-
+import React, { useEffect, useState } from "react";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField,
+  Button,
+} from "@mui/material";
 
 function UserForm({ user, onSave, onCancel }) {
-    const [formData, setFormData] = useState({ name: '', email: '' });
+  const [formData, setFormData] = useState({ name: "", email: "" });
 
-    useEffect(() => {
-        if (user) {
-            setFormData(user);
-        }
-    }, [user]);
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value })
+  useEffect(() => {
+    if (user) {
+      setFormData(user);
     }
+  }, [user]);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        onSave(formData);
-    }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-    return (
-        <Dialog open onClose={onCancel}>
-            <DialogTitle>{user._id ? 'Edit User' : 'Add User'}</DialogTitle>
-            <DialogContent>
-                <TextField
-                    autoFocus
-                    margin='dense'
-                    name='name'
-                    label='Name'
-                    type='text'
-                    fullWidth
-                    value={formData.name}
-                    onChange={handleChange}></TextField>
-                <TextField
-                    autoFocus
-                    margin='dense'
-                    name='email'
-                    label='Email'
-                    type='email'
-                    fullWidth
-                    value={formData.email}
-                    onChange={handleChange}></TextField>
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={onCancel} color='primary'>Cancel</Button>
-                <Button onClick={handleSubmit} color='primary'>Save</Button>
-            </DialogActions>
-        </Dialog>
-    )
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSave(formData);
+  };
+
+  return (
+    <Dialog open onClose={onCancel}>
+      <DialogTitle>{user.id ? "Edit User" : "Add User"}</DialogTitle>
+      <DialogContent>
+        <TextField
+          autoFocus
+          margin="dense"
+          name="name"
+          label="Name"
+          type="text"
+          fullWidth
+          value={formData.name}
+          onChange={handleChange}
+        ></TextField>
+        <TextField
+          autoFocus
+          margin="dense"
+          name="email"
+          label="Email"
+          type="email"
+          fullWidth
+          value={formData.email}
+          onChange={handleChange}
+        ></TextField>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onCancel} color="primary">
+          Cancel
+        </Button>
+        <Button onClick={handleSubmit} color="primary">
+          Save
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
 }
 
-export default UserForm
+export default UserForm;
